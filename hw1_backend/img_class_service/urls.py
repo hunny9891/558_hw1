2503445b7d4a9d4c029bf5458659b4b2d4029f8c
Himanshu Thakur <thakurh@uw.edu>
2022-01-27 20:59:54 -0800

    Final Changes Server Ready

--- hw1_backend/img_class_service/urls.py
+++ hw1_backend/img_class_service/urls.py
@@ -1,10 +1,6 @@
 from django.urls import include, path
-#from rest_framework import routers
 
 from . import views
-
-#router = routers.DefaultRouter()
-#router.register(r'images', views.ImageViewSet)
 
 urlpatterns = [
     path(r'api/images', views.transactions),
