d446b3163edcd58fda7e4cfaa887d1633b653729
Himanshu Thakur <thakurh@uw.edu>
2022-01-29 10:28:37 -0800

    aws changes

--- hw1_backend/hw1_backend/settings.py
+++ hw1_backend/hw1_backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -24,7 +25,7 @@ SECRET_KEY = 'django-insecure-t$8can@d3i(u9!2*wb1b96=6nutj2=g4%ka06=ox0vuipzx06(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['django-hw1-env.eba-bq2yu9vh.us-west-2.elasticbeanstalk.com']
 
 
 # Application definition
@@ -77,14 +78,27 @@ WSGI_APPLICATION = 'hw1_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'HOST': 'localhost',
-        'NAME': 'postgres',
-        'USER': 'ht',
-        'PASSWORD': 'P@55word',
+if 'RDS_DB_NAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
     }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'HOST': 'localhost',
+            'NAME': 'postgres',
+            'USER': 'ht',
+            'PASSWORD': 'P@55word',
+            'PORT': 5432
+        }
 }
 
 
